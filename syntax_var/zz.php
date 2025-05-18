e898835b1db0b8f6709876ec26db9d5a4247403b
0xZahidp <mdzahidhasanpatwary@gmail.com>
2025-05-18 23:16:08 +0600

    My fisrt php code

---
+++ syntax_var/zz.php
@@ -0,0 +1,11 @@
+<?php 
+$firstName = "Zahid";
+$lastName = "Patwary";
+$email = "zahid@nexgami.com";
+$age = 24;
+$money = 100000;
+echo "Hello MD $firstName Hasan $lastName . Your email is: $email , your age is: $age <br>";
+
+echo "Hello ". $firstName ." Hasan". $lastName .". Your email is ". $email .". Your age is: ". $age;
+
+?>
